1504e75062a98f644b5e4e7cec185dae0a0dd276
clcar23 <92751822+clcar23@users.noreply.github.com>
2022-01-20 19:55:26 -0700

    added changes to DjangoUniversity
    still throwing exception on the website submit

--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -8,9 +8,7 @@ class djangoClasses(models.Model):
     Instructor_Name = models.CharField(max_length=30)
     Duration = models.FloatField()
 
-# Added three objects from the class with attribute values
-course1 = djangoClasses("Underwater Basket Weaving", 101, "Alice Apple", 1.5 )
+    objects = models.Manager()
 
-course2 = djangoClasses("Python Problems", 102, "Bob Banana", 2.0)
-
-course3 = djangoClasses("SQL School", 103, "Carey Cabbage", 1.75)
+    def __str__(self):
+        return self.name
